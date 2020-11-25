3a1e2a6bba26d320866bb23aad8f8de5cff0db84
tuckerTU <tuckerishere@gmail.com>
2020-11-25 12:41:28 -0700

    commit tree 4857bed06e63b73a8ed6bdcb9f2d701d8fb4db52

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+      uri = URI.parse(@url)
+      open(uri).string
+    end
+
+    def parse_json
+        JSON.load(open(@url))
+    end
+
+end    
--- spec/get_requester_spec.rb
+++ spec/get_requester_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 JSON_STRING = "[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]"
 CONVERTED_DATA = [{ 'name' => 'Daniel', 'occupation' => 'LG Fridge Salesman' }, { 'name' => 'Joe', 'occupation' => 'WiFi Fixer' }, { 'name' => 'Avi', 'occupation' => 'DJ' }, { 'name' => 'Howard', 'occupation' => 'Mountain Legend' }].freeze
